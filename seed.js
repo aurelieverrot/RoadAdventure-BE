d8f42638f48e897b3f18e33dbf24d86e1f38de1a
aurelieverrot <aurelie.verrot@gmail.com>
2020-04-20 08:49:08 -0700

    last commit

--- seed.js
+++ seed.js
@@ -2,47 +2,11 @@ require('dotenv').config()
 const db = require("./models");
 
 const trips = [
-  {
-    title: 'Trip 1',
-    user: '5e98bdd2c95377b1861b14d1',
-    status: false,
-    shortText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore"
-  },
-  {
-    title: 'Trip 2',
-    
-    status: true,
-    shortText: "magna aliqua. Sollicitudin tempor id eu nisl. Cras sed felis eget velit aliquet sagittis id. Et sollicitudin ac orci"
-  },
-  {
-    title: 'Trip 4',
-    status: false,
-    shortText: "phasellus. Ultrices neque ornare aenean euismod. Metus dictum at tempor commodo ullamcorper. Hendrerit dolor"
-  },
-  {
-    title: 'Trip 5',
-    user: '5e98bdd2c95377b1861b14d1',
-    status: true,
-    shortText: "magna eget est lorem. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Turpis"
-  },
-  {
-    title: 'Trip 6',
-    status: false,
-    shortText: "egestas integer eget aliquet nibh praesent tristique. Consequat semper viverra nam libero justo laoreet sit"
-  }
+  
 ]
 
 const users = [
-  {
-    username: 'Lili',
-    email: 'lili@lili.com',
-    password: '1234'
-  },
-  {
-    username: 'Aurelie',
-    email: 'aurelie@ga.com',
-    password: '5678'
-  }
+  
 ]
 
 
@@ -72,24 +36,24 @@ db.Trip.deleteMany({}, (err, result) => {
 
 
     // Delete All Users
-    // console.log('Deleting all users...');
+    console.log('Deleting all users...');
 
-    // db.User.deleteMany({}, (err, result) => {
-    //   if (err) {
-    //     console.log(err);
-    //     process.exit();
-    //   }
-    //   console.log(`Successfully deleted ${result.deletedCount} users.`);
+    db.User.deleteMany({}, (err, result) => {
+      if (err) {
+        console.log(err);
+        process.exit();
+      }
+      console.log(`Successfully deleted ${result.deletedCount} users.`);
 
 
       // Create New Trips
-      console.log('Creating new trips...');
-      db.Trip.create(trips, (err, newTrips) => {
-        if (err) {
-          console.log(err);
-          process.exit();
-        }
-        console.log(`Successfully created ${newTrips.length} trips.`);
+      // console.log('Creating new trips...');
+      // db.Trip.create(trips, (err, newTrips) => {
+      //   if (err) {
+      //     console.log(err);
+      //     process.exit();
+      //   }
+      //   console.log(`Successfully created ${newTrips.length} trips.`);
         
 
         // Create New Users
